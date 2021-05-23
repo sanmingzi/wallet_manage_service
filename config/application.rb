119399927ef842c4de7ffe834b8db3cc8a62ee5a
sanmingzi <zhiming.zhong@outlook.com>
2021-05-24 02:41:15 +0800

    use config.eager_load_paths instead of autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,6 @@ module WMS
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.autoload_paths << config.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
